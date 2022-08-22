b95ad76eb7931719ca57bc4a43cce27213a59309
mpvasco <paulavascoo@gmail.com>
2022-08-22 19:33:20 -0300

    Create & Read are working

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+
+
+app = Flask(__name__)
+
+# Configure db
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '12345'
+app.config['MYSQL_DB'] = 'crudflask'
+
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Fetch form data
+        userDetails = request.form
+        name = userDetails['name']
+        password = userDetails['password']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(userName, userPassword) VALUES(%s, %s)",(name, password))
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/users')
+    return render_template('index.html')
+
+@app.route('/users')
+def users():
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM users")
+    if resultValue > 0:
+        userDetails = cur.fetchall()
+        return render_template('users.html',userDetails=userDetails)
+
+if __name__ == '__main__':
+    app.run(debug=True)
