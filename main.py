2907a2806ffa5b268bc2e0116f29f534b231565d
mpvasco <paulavascoo@gmail.com>
2022-08-23 13:55:04 -0300

    update is working

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
 
 
@@ -10,10 +10,12 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '12345'
 app.config['MYSQL_DB'] = 'crudflask'
 
+
+
 mysql = MySQL(app)
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def create():
     if request.method == 'POST':
         # Fetch form data
         userDetails = request.form
@@ -23,18 +25,63 @@ def index():
         cur.execute("INSERT INTO users(userName, userPassword) VALUES(%s, %s)",(name, password))
         mysql.connection.commit()
         cur.close()
-        return redirect('/users')
-    return render_template('index.html')
+        return redirect('/read')
+    return render_template('create.html')
 
-@app.route('/users')
-def users():
+@app.route('/read')
+def read():
     cur = mysql.connection.cursor()
     resultValue = cur.execute("SELECT * FROM users")
-    if resultValue > 0:
+    if resultValue:
         userDetails = cur.fetchall()
-        return render_template('users.html',userDetails=userDetails)
+        return render_template('read.html',userDetails=userDetails)
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM users where userID = "+ str(id) +";")
+    resultValue= cur.fetchall()
+    mysql.connection.commit()
+    cur.close()
+    if request.method=='GET':
+      return render_template('update.html', x=resultValue )
+
+    if request.method=='POST':
+      userName = request.form['name']
+      cur = mysql.connection.cursor()
+      query = "UPDATE users SET userName = %s WHERE userID = %s;"
+      cur.execute(query, (userName, id))
+      resultValue= cur.fetchall()
+      print(resultValue)
+      mysql.connection.commit()
+      cur.close()
+    return redirect('/read')
+
+
+@app.route('/delete/<int:id>', methods=['GET', 'POST'])
+def delete(id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM users where userID = "+ str(id) +";")
+    resultValue= cur.fetchall()
+    mysql.connection.commit()
+    cur.close()
+    if request.method=='GET':
+      return render_template('delete.html', x=resultValue )
+
+    if request.method=='POST':
+      cur = mysql.connection.cursor()
+      query = "DELETE FROM users WHERE userID = %s;"
+      cur.execute(query, id)
+      resultValue= cur.fetchall()
+      print(resultValue)
+      mysql.connection.commit()
+      cur.close()
+    return redirect('/read')
+
+
+
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
